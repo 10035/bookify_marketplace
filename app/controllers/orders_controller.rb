9ec14396b771f8dff5a713acf80186be3352406c
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-02 15:14:59 -0800

    Created orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+
+  def create
+    # Book.new()
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get 'books/create'
+
   resources :books do
     resources :orders, only: [:create]
   end
