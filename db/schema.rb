2f21dafc21803ee271b6006911ec4d638b433ff1
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-05 18:47:38 -0800

    seed set

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_055718) do
+ActiveRecord::Schema.define(version: 2021_02_06_021411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_055718) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id", null: false
+    t.string "img_url"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
