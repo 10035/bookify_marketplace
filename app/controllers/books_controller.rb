b9d02f2e2ee873bc0e686d60edb5ac92208adf82
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-06 15:38:34 -0800

    Set up cloudinary and tested image upload for models

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -44,6 +44,6 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title, :published_year, :genre, :price, :description)
+    params.require(:book).permit(:title, :published_year, :genre, :price, :description, :photo)
   end
 end
