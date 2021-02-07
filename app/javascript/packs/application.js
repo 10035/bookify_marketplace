12ee271f255eeee4cef084fd02ff8ce409ef3c33
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-07 14:21:48 -0800

    Working on calendar display bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,10 +30,10 @@ import "flatpickr";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
+// document.addEventListener('turbolinks:load', () => {
+//   // Call your functions here, e.g:
+//   // initSelect2();
+// });
 
 document.addEventListener('DOMContentLoaded', function() {
   flatpickr('#book-order-dates');
