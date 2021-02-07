42ab1b5258a2255e9e1959645eb6e0a76a8d352b
Kathy Tavia <kathy.tavia@gmail.com>
2021-02-06 16:21:47 -0800

    removed img url column from books table and started updating seed to link with cloudinary

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :orders
   has_many :reviews, through: :orders
-  has_one_attached :photo
+  has_many_attached :photos
 
   # def unavailable_dates
   #   orders.pluck(:start_date, :end_date).map do |range|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
-img_urls = ["https://i.picsum.photos/id/722/200/200.jpg?hmac=wNug9Ox95uwU6niL7InSfuJXj6KQLckDilJExPwv75Q", "https://i.picsum.photos/id/195/200/200.jpg?hmac=XbxBxpgP4z7t-dGvrqGicWMyRkF_t0suZz3RqiOsY64", "https://i.picsum.photos/id/880/200/200.jpg?hmac=g5VV-eqqKk9TdTvkzKu6PzjRtzrqVhrj6v7H9ZT7PDo" ,"https://i.picsum.photos/id/659/200/200.jpg?hmac=kFpdD3XTBGwPUAH1tD-AiWigstjIX8WGIcyySuVQIvE", "https://i.picsum.photos/id/1/200/200.jpg?hmac=jZB9EZ0Vtzq-BZSmo7JKBBKJLW46nntxq79VMkCiBG8", "https://i.picsum.photos/id/134/200/200.jpg?hmac=a3L-JjVSGeG8w3SdNpzxdh8WSC0xHJXgeD6QryCK7pU", "https://i.picsum.photos/id/194/200/200.jpg?hmac=f1VYjvgDG_6vPwJyTb-Xl1HpXKM23stmhFUnmPE_yL8", "https://i.picsum.photos/id/556/200/200.jpg?hmac=5uOJ4fW7ElE2P5NfHlvz2zx4d99Ts2-lxy8tucygHLc", "https://i.picsum.photos/id/505/200/200.jpg?hmac=c295sjTIAZ_9Gj-PENrzAbATNIiWPL1dmhIhWndYnyo", "https://i.picsum.photos/id/66/200/200.jpg?hmac=gaKXe-rWmo5fSEm79TTkW_yFJLECw3FdRCh6Dm7jp4g", "https://i.picsum.photos/id/203/200/200.jpg?hmac=fydyJjsULq7iMwTTIg_m6g_PQQ1paJrufNsEiqbJRsg"]
+puts "Deleting seed"
+
+Author.destroy_all
+Book.destroy_all
+User.destroy_all
+
+puts "Seeding starting..."
 
 10.times {
   Author.create!(
@@ -16,15 +23,18 @@ img_urls = ["https://i.picsum.photos/id/722/200/200.jpg?hmac=wNug9Ox95uwU6niL7In
 }
 
 10.times {
-  Book.create!(
+  file = URI.open('https://picsum.photos/300/300')
+
+  book = Book.new(
     title: Faker::Book.title,
     published_year: [1920...2020].sample,
     genre: Faker::Book.genre,
-    price: Faker::Number.decimal(l_digits: 2),
+    price: Faker::Number.decimal(l_digits: 2, r_digits: 2),
     description: Faker::Lorem.paragraphs,
-    img_url: img_urls.sample,
     author_id: Author.all.map {|author| author.id}.sample
     )
+  book.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+  book.save
 }
 
 2.times {
@@ -33,3 +43,10 @@ img_urls = ["https://i.picsum.photos/id/722/200/200.jpg?hmac=wNug9Ox95uwU6niL7In
     password: 12345678
     )
 }
+
+
+puts Author.count
+puts Book.count
+puts User.count
+
+puts "Seeding complete"
