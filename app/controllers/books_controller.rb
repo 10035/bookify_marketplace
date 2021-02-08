b4fb47ea65b12441acaccc9edaa0de2a12827205
Rajeev Balendra <rj.balendra@gmail.com>
2021-02-08 21:07:02 +1100

    css improvements

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,11 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    if params[:query].present?
+      @books = Book.where(title: params[:query])
+    else
+      @books = Book.all
+    end
     # @books = policy_scope(Book).orders(created_at: :desc)
   end
 
